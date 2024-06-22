5a5db46737fff1e66bb3c83b2433245a46e5a4ff
command0929 <142138531+command0929@users.noreply.github.com>
2024-06-22 14:53:48 +0900

    Naver login 1.0

---
+++ bvsd_templet.js
@@ -0,0 +1,124 @@
+module.exports = {
+    "a": "7cd12780-773f-4d15-a70f-8a7324980126-0",
+    "b": "1.3.9",
+    "c": false,
+    "d": [{
+        "i": "id",
+        "a": [],
+        "b": {
+            "a": ["0,asd"],
+            "b": 0
+        },
+        "c": "",
+        "d": "asd",
+        "e": false,
+        "f": false
+    }, {
+        "i": "pw",
+        "a": [],
+        "b": {
+            "a": ["0,"],
+            "b": 0
+        },
+        "c": "",
+        "d": "",
+        "e": true,
+        "f": false
+    }],
+    "e": {
+        "a": {
+            "a": 444,
+            "b": 444,
+            "c": 444
+        },
+        "b": {
+            "a": 444,
+            "b": 444,
+            "c": 444
+        }
+    },
+    "f": {
+        "a": {
+            "a": {
+                "a": 444,
+                "b": 444,
+                "c": 444
+            },
+            "b": {
+                "a": 444,
+                "b": 444,
+                "c": 444
+            }
+        },
+        "b": {
+            "a": {
+                "a": 444,
+                "b": 444,
+                "c": 444
+            },
+            "b": {
+                "a": 444,
+                "b": 444,
+                "c": 444
+            }
+        }
+    },
+    "g": {
+        "a": ["0|4205|127|166", "0|63|1|0", "0|46|1|2", "0|7|1|2", "0|10|0|3", "0|7|1|2", "0|8|0|2", "0|7|2|3", "0|8|1|2", "0|12|0|2", "0|5|1|2", "0|7|0|1", "0|16|1|1", "0|24|0|1", "0|8|0|1", "0|8|1|2", "0|9|0|2", "0|7|1|2", "0|8|1|4", "0|8|0|4", "0|8|1|2", "0|8|0|2", "0|8|1|2", "1|172|0|0", "0|42|0|0", "0|17|0|0", "0|14|0|0", "2|51|0|0", "0|298|3|2", "0|7|1|3", "0|7|2|4", "0|8|2|2", "0|10|3|4", "0|6|3|3", "0|8|4|3", "0|8|3|3", "0|1136|14|15", "0|897|0|2", "0|16|0|1", "0|55|0|2", "0|8|0|1", "0|16|0|1", "0|31|0|2", "0|0|2|2", "0|9|1|2", "0|8|0|1", "0|24|1|2", "0|8|0|1", "0|8|0|1", "0|9|0|2", "0|7|0|1", "0|8|2|2", "0|8|1|3", "0|16|1|3", "0|8|0|2", "0|8|0|4", "0|8|1|3", "0|8|1|1", "0|8|1|2", "0|16|0|1", "0|8|1|1", "0|8|0|1", "0|8|0|1", "0|9|0|2", "0|15|0|2", "0|8|2|2", "0|9|0|1", "0|7|1|3", "0|8|1|1", "0|17|0|3", "0|15|0|2", "0|9|0|2", "0|23|0|3", "0|8|2|1", "0|9|0|1", "0|7|1|2", "0|8|0|1", "1|210|0|0", "2|102|0|0"],
+        "b": 78,
+        "c": 195,
+        "d": 320,
+        "e": 7952,
+        "f": 0
+    },
+    "j": 3903,
+    "h": "216cf4295995a850c5cd2defb41abf5b",
+    "i": {
+        "a": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        "b": "ko-KR",
+        "c": 24,
+        "d": 4,
+        "e": 1.25,
+        "f": 4,
+        "g": [1536, 864],
+        "h": [1536, 824],
+        "i": -540,
+        "j": 1,
+        "k": 1,
+        "l": 1,
+        "m": "unknown",
+        "n": "Win32",
+        "o": "unknown",
+        "aa": ["PDF Viewer::Portable Document Format::application/pdf~pdf,text/pdf~pdf", "Chrome PDF Viewer::Portable Document Format::application/pdf~pdf,text/pdf~pdf", "Chromium PDF Viewer::Portable Document Format::application/pdf~pdf,text/pdf~pdf", "Microsoft Edge PDF Viewer::Portable Document Format::application/pdf~pdf,text/pdf~pdf", "WebKit built-in PDF::Portable Document Format::application/pdf~pdf,text/pdf~pdf"],
+        "p": "9bfe89a9e84c3567565cceead18fa8aa",
+        "q": "e521e02c8b4772a8485c21406dd24d94",
+        "r": "Google Inc. (Intel)~ANGLE (Intel, Intel(R) HD Graphics Family (0x00000A16) Direct3D11 vs_5_0 ps_5_0, D3D11)",
+        "s": false,
+        "t": false,
+        "u": false,
+        "v": false,
+        "w": false,
+        "x": [0, false, false],
+        "aca": "x86",
+        "acb": "",
+        "acc": "64",
+        "acd": "Windows",
+        "ace": "10.0.0",
+        "acf": "122.0.6261.69",
+        "acg": ["{\"brand\":\"Chromium\",\"version\":\"122.0.6261.69\"}", "{\"brand\":\"Not(A:Brand\",\"version\":\"24.0.0.0\"}", "{\"brand\":\"Google Chrome\",\"version\":\"122.0.6261.69\"}"],
+        "ach": false,
+        "aci": false,
+        "ad": "5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        "ae": "true",
+        "af": "true",
+        "ak": "default|prompt",
+        "ag": "1",
+        "aj": "782|831",
+        "ah": "false",
+        "ai": "50",
+        "am": "portrait",
+        "an": "false",
+        "al": "",
+        "y": ["Arial", "Arial Black", "Arial Narrow", "Arial Unicode MS", "Book Antiqua", "Bookman Old Style", "Calibri", "Cambria", "Cambria Math", "Century", "Century Gothic", "Century Schoolbook", "Comic Sans MS", "Consolas", "Courier", "Courier New", "Garamond", "Georgia", "Helvetica", "Impact", "Lucida Bright", "Lucida Calligraphy", "Lucida Console", "Lucida Fax", "Lucida Handwriting", "Lucida Sans", "Lucida Sans Typewriter", "Lucida Sans Unicode", "Microsoft Sans Serif", "Monotype Corsiva", "MS Gothic", "MS PGothic", "MS Reference Sans Serif", "MS Sans Serif", "MS Serif", "MYRIAD PRO", "Palatino Linotype", "Segoe Print", "Segoe Script", "Segoe UI", "Segoe UI Light", "Segoe UI Semibold", "Segoe UI Symbol", "Tahoma", "Times", "Times New Roman", "Trebuchet MS", "Verdana", "Wingdings", "Wingdings 2", "Wingdings 3"]
+    }
+}
---
+++ index.js
@@ -0,0 +1,82 @@
+importClass(java.security.spec.RSAPublicKeySpec)
+importClass(java.security.KeyFactory)
+importClass(javax.crypto.Cipher)
+importClass(org.jsoup.Jsoup)
+importClass(java.util.UUID)
+importClass(java.math.BigInteger)
+importClass(java.lang.StringBuilder)
+importClass(org.jsoup.Connection)
+
+module.exports = function naver_login(id, pw) {
+    const dom = Jsoup.connect("https://nid.naver.com/nidlogin.login?svctype=262144").get()
+
+    const dynamic = dom.select("#dynamicKey").get(0).attr("value")
+    const keyString = dom.select("#session_keys").get(0).attr("value")
+
+    const keys = keyString.split(",")
+    const encnm = keys[1]
+    const encpw = encrypt(keys, id, pw)
+    const bvsd = makeBvsd(id)
+
+    return Jsoup.connect("https://nid.naver.com/nidlogin.login")
+      .data("localechange", "")
+      .data("dynamicKey", dynamic)
+      .data("encpw", encpw)
+      .data("enctp", "1")
+      .data("svctype", "262144")
+      .data("smart_LEVEL", "-1")
+      .data("bvsd", bvsd)
+      .data("encnm", encnm)
+      .data("locale", "ko_KR")
+      .data("url", "https://m.naver.com")
+      .data("id", "")
+      .data("pw", "")
+      .data("nvlong", "on")
+      .method(Connection.Method.POST)
+      .execute().cookies()
+  };
+
+  function encrypt(keys, id, pw) {
+    const n = new BigInteger(keys[2], 16)
+    const e = new BigInteger(keys[3], 16)
+
+    const spec = new RSAPublicKeySpec(n, e)
+
+    const factory = KeyFactory.getInstance("RSA")
+    const key = factory.generatePublic(spec)
+
+    const cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding")
+    cipher.init(Cipher.ENCRYPT_MODE, key)
+
+    const plain = new java.lang.String(getLenChar(keys[0]) + keys[0] + getLenChar(id) + id + getLenChar(pw) + pw)
+
+    const bytes = cipher.doFinal(plain.getBytes())
+    return bytesToHex(bytes)
+  }
+
+  function getLenChar(str) {
+    return String.fromCharCode(str.length)
+  }
+
+  function bytesToHex(bytes) {
+    const builder = new StringBuilder()
+    for (let b of bytes) {
+      builder.append(java.lang.String.format("%02x", new java.lang.Byte(b)))
+    }
+
+    return builder.toString()
+  }
+
+  function makeBvsd(id) {
+    const bvsd = require("./bvsd_templet")
+    const uuid = UUID.randomUUID() + "-0"
+
+    bvsd.a = uuid
+    bvsd.d[0].b.a = ["0," + id]
+    bvsd.d[0].d = id
+
+    return JSON.stringify({
+      "uuid": uuid,
+      "encData": require("./lz-string.min").compressToEncodedURIComponent(JSON.stringify(bvsd))
+    })
+  }
